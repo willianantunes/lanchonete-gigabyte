7e4e2fa6961cdb73b2a371d73db4fdc2ab3430a4
Willian Antunes <willian.lima.antunes@gmail.com>
2022-08-29 19:57:44 -0300

    feat(leetcode): add better solutions for 1 and 88

--- leetcode/lc88_merge_sorted_array.py
+++ leetcode/lc88_merge_sorted_array.py
@@ -10,39 +10,35 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if (m == 1 and n == 0) or (m > 1 and n == 0):
+        if m > 0 and n == 0:
             return
-        if m == 0 and n == 1:
-            nums1[0] = nums2[0]
-            return
-        if m == 0 and n > 1:
+        if m == 0 and n > 0:
             for index, value in enumerate(nums2):
                 nums1[index] = value
             return
+        pointer_nums1 = 0
+        pointer_nums2 = 0
+        total = m + n
 
-        number_of_entries = m + n
-        pointer_left, pointer_right = 0, 0
+        while pointer_nums1 < total:
+            current_nums1_value = nums1[pointer_nums1]
+            current_nums2_value = nums2[pointer_nums2]
 
-        while pointer_left < number_of_entries:
-            current_nums1_value = nums1[pointer_left]
-            current_nums2_value = nums2[pointer_right]
-
-            if pointer_left >= m:
-                nums1[pointer_left] = current_nums2_value
-                pointer_right += 1
-            elif current_nums1_value > current_nums2_value:
-                nums1[pointer_left] = current_nums2_value
-                nums2[pointer_right] = current_nums1_value
-                for sorting_pointer_limit in range(n - 1, 0, -1):
-                    for sorting_pointer_left in range(sorting_pointer_limit):
-                        sorting_pointer_right = sorting_pointer_left + 1
-                        if nums2[sorting_pointer_left] > nums2[sorting_pointer_right]:
-                            nums2[sorting_pointer_left], nums2[sorting_pointer_right] = (
-                                nums2[sorting_pointer_right],
-                                nums2[sorting_pointer_left],
-                            )
-
-            pointer_left += 1
+            if pointer_nums1 < m:
+                if current_nums1_value <= current_nums2_value:
+                    pointer_nums1 += 1
+                else:
+                    nums1[pointer_nums1] = current_nums2_value
+                    nums2[pointer_nums2] = current_nums1_value
+                    pointer_nums1 += 1
+            else:
+                nums1[pointer_nums1] = current_nums2_value
+                nums2[pointer_nums2] = 0
+                pointer_nums1 = 0
+                pointer_nums2 += 1
+                m += 1
+                if m == total:
+                    break
 
 
 class TestSolution(unittest.TestCase):
