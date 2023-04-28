956cbdab4e183775120623533a29dbfe67f1b813
Willian Antunes <willian.lima.antunes@gmail.com>
2023-04-28 13:46:41 -0300

    chore(leetcode): change solution for 102

--- leetcode/lc102_binary_tree_level_order_traversal.py
+++ leetcode/lc102_binary_tree_level_order_traversal.py
@@ -11,40 +11,49 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return str(self.val)
+
+    def __repr__(self):
+        return str(self.val)
+
 
 class Solution:
     def levelOrder(self, root: TreeNode | None) -> list[list[int]]:
         if not root:
             return []
 
-        class LevelHolder:
-            pass
+        queue = [root]
+        nodes_grouped_by_level = []
 
-        evaluation = {}
-        level_holder = LevelHolder()
-        level_holder.current = -1
+        while queue:
+            found_nodes = []
+            number_of_nodes_to_be_seen = len(queue)
+            for _ in range(number_of_nodes_to_be_seen):
+                node = queue.pop(0)
+                found_nodes.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            nodes_grouped_by_level.append(found_nodes)
 
-        def _navigate_tree(node):
-            if node:
-                level_holder.current += 1
-                found_values = evaluation.get(level_holder.current, [])
-                found_values.append(node.val)
-                evaluation[level_holder.current] = found_values
-                _navigate_tree(node.left)
-                _navigate_tree(node.right)
-                level_holder.current -= 1
-
-        _navigate_tree(root)
-
-        level = []
-        for index in evaluation.keys():
-            level.append([])
-            level[index] = evaluation[index]
-
-        return level
+        return nodes_grouped_by_level
 
 
 class TestSolution(unittest.TestCase):
+    def generate_tree(self, config: list[int | None]) -> TreeNode:
+        nodes = [TreeNode(value) if value is not None else None for value in config]
+        children = nodes[::-1]
+        root = children.pop()
+        for node in nodes:
+            if node:
+                if children:
+                    node.left = children.pop()
+                if children:
+                    node.right = children.pop()
+        return root
+
     def setUp(self) -> None:
         self.solution = Solution()
 
@@ -69,3 +78,11 @@ class TestSolution(unittest.TestCase):
         node_9 = TreeNode(9)
         root = TreeNode(3, left=node_9, right=node_20)
         self.assertEqual([[3], [9, 20], [15, 7]], self.solution.levelOrder(root))
+
+    def test_example_4(self):
+        root = TreeNode(33)
+        self.assertEqual([[33]], self.solution.levelOrder(root))
+
+    def test_example_5(self):
+        root = self.generate_tree([2, None, 1, 1, 1, 3, 4, 1, 1, 10, 1, 8, 33, 31, 1, 24, 1])
+        self.assertEqual([[2], [1], [1, 1], [3, 4, 1, 1], [10, 1, 8, 33, 31, 1, 24, 1]], self.solution.levelOrder(root))
