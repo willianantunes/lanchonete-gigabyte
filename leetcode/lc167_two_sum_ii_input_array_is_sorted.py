861d2ff601ee5ae915e12667c1bdf59616cc290c
Willian Antunes <willian.lima.antunes@gmail.com>
2023-05-25 20:38:47 -0300

    chore(leetcode): change solution for 167

--- leetcode/lc167_two_sum_ii_input_array_is_sorted.py
+++ leetcode/lc167_two_sum_ii_input_array_is_sorted.py
@@ -2,27 +2,25 @@
 Solution for LC#167: Two Sum II - Input Array Is Sorted
 https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 """
+import bisect
 import unittest
+
+
+def find_index(array: list, target: int):
+    array_length = len(array)
+    expected_index = bisect.bisect_left(array, target)
+    if array_length > expected_index and array[expected_index] == target:
+        return expected_index
 
 
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        numbers_length = len(numbers)
-        if numbers_length == 2:
-            return [1, 2]
-
-        left_pointer = 0
-        right_pointer = numbers_length - 1
-        while left_pointer < right_pointer:
-            left_value = numbers[left_pointer]
-            right_value = numbers[right_pointer]
-            total = left_value + right_value
-            if total < target:
-                left_pointer += 1
-            elif total > target:
-                right_pointer -= 1
-            else:
-                return [left_pointer + 1, right_pointer + 1]
+        for index, value in enumerate(numbers):
+            value_to_be_found = -1 * (value + (-1 * target))
+            found_index = find_index(numbers, value_to_be_found)
+            if found_index is not None and index != found_index:
+                indexes = [index + 1, found_index + 1] if index < found_index else [found_index + 1, index + 1]
+                return indexes
 
 
 class TestSolution(unittest.TestCase):
@@ -43,3 +41,13 @@ class TestSolution(unittest.TestCase):
         numbers = [-1, 0]
         target = -1
         self.assertEqual([1, 2], self.solution.twoSum(numbers, target))
+
+    def test_example_4(self):
+        numbers = [0, 0, 3, 4]
+        target = 0
+        self.assertEqual([1, 2], self.solution.twoSum(numbers, target))
+
+    def test_example_5(self):
+        numbers = [5, 25, 75]
+        target = 100
+        self.assertEqual([2, 3], self.solution.twoSum(numbers, target))
